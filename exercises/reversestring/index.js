d49bdba27c0fbc0895f79f48a57ee8977366bae7
Maruf Siddiqui <maruf.siddiqui@shore.com>
2017-11-30 16:13:18 +0100

    Prettify reversestring.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,35 +7,38 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reduce((a, i) => i + a, '');
+  return str.split("").reduce((a, i) => i + a, "");
 }
 
 function reverse1(str) {
-    let arr = [];
-    for (let i of str) {
-        arr.unshift(i);
-    }
-    return arr.join('');
+  let arr = [];
+  for (let i of str) {
+    arr.unshift(i);
+  }
+  return arr.join("");
 }
 
 function reverse2(str) {
-    let arr = []
-    for (let i = 0; i < str.length; i++) {
-        arr.unshift(str[i]);
-    }
-    return arr.join('');
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    arr.unshift(str[i]);
+  }
+  return arr.join("");
 }
 
 function reverse3(str) {
-    return arr.split('').reverse().join('');
+  return arr
+    .split("")
+    .reverse()
+    .join("");
 }
 
 function reverse4(str) {
-    let reversed = '';
-    for (let char of str) {
-        reversed = char + reversed;
-    }
-    return reversed;
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
 }
 
 module.exports = reverse;
