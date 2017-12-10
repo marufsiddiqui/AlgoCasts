dfb835264824893d991eaa85b57cc460e1963e9d
Maruf Siddiqui <maruf.siddiqui@shore.com>
2017-12-10 11:11:40 +0100

    Add anagram solution # 2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,7 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
+function anagrams(str1, str2) {
+  const str1CharMap = charMap(str1);
+  const str2CharMap = charMap(str2);
+
+  if (Object.keys(str1CharMap).length !== Object.keys(str2CharMap).length) {
+    return false;
+  }
+
+  for (let char in str1CharMap) {
+    if (str1CharMap[char] !== str2CharMap[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function charMap(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .reduce((a, i) => {
+      a[i] = a[i] + 1 || 1;
+      return a;
+    }, {});
+}
+
+function anagrams1(stringA, stringB) {
   return toSortedString(stringA) === toSortedString(stringB);
 }
 
