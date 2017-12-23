ba601a6ffec137751f32d772d0d06c530ddfea42
Maruf Siddiqui <maruf.siddiqui@shore.com>
2017-12-23 14:36:09 +0100

    Add q from stack solution #1

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,35 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.inbox = new Stack();
+    this.outbox = new Stack();
+  }
+
+  add(item) {
+    this.inbox.push(item);
+  }
+
+  fillOutbox() {
+    if (!this.outbox.peek()) {
+      while (this.inbox.peek()) {
+        this.outbox.push(this.inbox.pop());
+      }
+    }
+  }
+
+  remove() {
+    this.fillOutbox();
+    return this.outbox.pop();
+  }
+
+  peek() {
+    this.fillOutbox();
+    return this.outbox.peek();
+  }
+}
 
 module.exports = Queue;
